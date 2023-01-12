d06f1b0cca536fad2b2dd71027dcb400841c02fc
Eitan Reiffman <eitanreiffman@gmail.com>
2023-01-12 09:47:25 -0500

    Created Database and Models

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
+from app.models import User
 
 @app.route('/')
 def index():
@@ -18,7 +19,15 @@ def signup():
         password = form.password.data
         print(first_name, last_name, email, username, password)
 
-        flash(f'Thank you, {first_name} {last_name}, for signing up to our address book!')
+        check_user = User.query.filter( (User.username == username) | (User.email == email) ).all()
+
+        if check_user:
+            flash('Sorry! A user with that email and/or username already exists', 'danger')
+            return redirect(url_for('signup'))
+
+        new_user = User(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+
+        flash(f'Thank you, {new_user.first_name} {new_user.last_name}, for signing up to our address book!', 'success')
 
         return redirect(url_for('index'))
 
